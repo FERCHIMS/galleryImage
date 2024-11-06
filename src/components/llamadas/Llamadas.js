ae13d9d6c0a65997bb6f35843df1d4ae16b93456
FERCHIMS <gr.egfredderick@gmail.com>
2024-11-06 13:08:57 -0300

    pruebas de rendimiento

--- src/components/llamadas/Llamadas.js
+++ src/components/llamadas/Llamadas.js
@@ -1,7 +1,7 @@
 
 export default async function getPhotos() {
     try {
-        const res = await fetch("https://greftom4.github.io/apiEstaticaProds/modelos.json",{cache: "force-cache"} );
+        const res = await fetch("https://greftom4.github.io/apiEstaticaProds/advertising.json",{cache: "force-cache"} );
         const data = res.json();
         return data
     }catch(error) {

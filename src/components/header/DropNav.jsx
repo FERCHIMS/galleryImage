d1238781c5f45b507ef8242af641ed9101c7830d
FERCHIMS <gr.egfredderick@gmail.com>
2024-11-16 10:28:21 -0300

    agregado de transition al drop

--- src/components/header/DropNav.jsx
+++ src/components/header/DropNav.jsx
@@ -1,6 +1,7 @@
 'use client'
 import Link from "next/link";
 import { useState, useEffect, useRef } from "react"
+import { motion, AnimatePresence } from "framer-motion";
 
 const links = [
     {
@@ -29,23 +30,35 @@ const DropNav = () => {
     }
 
     return (
-        <div className="block lg:hidden fixed w-full z-50 ">
-            <div onClick={handleDrop} className="bg-pink-300  w-[50px] h-[50px] flex justify-center items-center ">
-                <div className="z-50 bg-pink-300 w-fit h-fit">
+        <div className={`block lg:hidden fixed w-full h-screen z-50  overflow-hidden delay-300 ${isDrop ? "pointer-events-auto" : "pointer-events-none"} `} >
+            <div onClick={handleDrop} className="bg-pink-300 pointer-events-auto  w-[50px] h-[50px] flex justify-center items-center ">
+                <div className="z-50 bg-pink-300 w-[50px] h-[50px] flex justify-center items-center">
                     <h1 className="text-white  text-[2rem]">X</h1>
                 </div>
             </div>
-            {isDrop && (
-                <div className={`z-30 absolute transition-all inset-0 duration-700 ${isDrop ? "h-screen bg-green-300" : "h-0 bg-orange-300"} `}>
-                    <div className="flex flex-col justify-normal items-center">
+
+            <AnimatePresence>
+                <div className={`z-30 absolute transition-transform h-screen inset-0 duration-700  ${isDrop ? "translate-y-[0%] pointer-events-auto" : "translate-y-[100%] pointer-events-none"} `}>
+                    <div
+
+                        className="flex flex-col justify-normal items-center h-screen bg-black">
                         {links.map((link) => (
                             <li onClick={removeDrop} key={link.title} className="flex">
-                                <Link  className="text-[6rem]" href={link.href} >{link.title} </Link>
+                                <Link className="text-[3rem] text-white font-black delay-200 transition-all duration-[1200ms]" href={link.href} >
+                                <motion.h2
+                                initial={ { opacity: 0 }}
+                                animate={isDrop ? { opacity: 1 } : { opacity: 0 } }
+                                exit={{ opacity: 0 }}
+                                transition={{ duration: isDrop ? 0.8 : 0.3, ease: 'easeInOut'}}
+                                >{link.title}
+                                </motion.h2>
+                                </Link>
                             </li>
                         ))}
                     </div>
                 </div>
-            )}
+            </AnimatePresence>
+
         </div>
     )
 }
